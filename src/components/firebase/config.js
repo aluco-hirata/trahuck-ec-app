e118a31559b0e5d015b091b068ba07a5bf7ae42c
hirata <>
2020-07-05 18:02:41 +0900

    #2 start

---
+++ src/components/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCUOvpkTylRlXRvix8Vvvl6iw7JR2q5EP8",
+  authDomain: "ec-app-95c65.firebaseapp.com",
+  databaseURL: "https://ec-app-95c65.firebaseio.com",
+  projectId: "ec-app-95c65",
+  storageBucket: "ec-app-95c65.appspot.com",
+  messagingSenderId: "736706478174",
+  appId: "1:736706478174:web:638deb8041db501d5cf4cf",
+  measurementId: "G-VPSCB7266B"
+};
